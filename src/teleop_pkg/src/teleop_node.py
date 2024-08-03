9612079e5cc63d41df6040fec9e2748c41ca8ddc
Quan <quan.pham@tum.de>
2024-08-04 00:54:56 +0200

    delete unneccessary argument

--- src/teleop_pkg/src/teleop_node.py
+++ src/teleop_pkg/src/teleop_node.py
@@ -41,7 +41,7 @@ class TeleopControllerNode:
             quit() # handle ctrl+C        
         return ch
 
-    def control_loop(self, event):
+    def control_loop(self, ):
         msg = Actuators()
         key = self.getchar()
         if key == 'w':
